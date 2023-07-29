5173fe92ecd5d4059ce0ac94c7236d8b791c1b36
bezachka <ilyaershov135@gmail.com>
2023-07-29 16:11:18 +0300

    commit

--- advertisment/app_advertisment/models.py
+++ advertisment/app_advertisment/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from django.contrib import admin
 
@@ -10,8 +11,8 @@ class Advertisment(models.Model):
     user = models.CharField('Пользователь', max_length=126)
     auction_test = models.BooleanField('торг', help_text='Возможен торг или нет', default=False)
     description = models.TextField('Описание')
-    created_at = models.DateField(auto_now_add=True)
-    update_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
     
 
 
@@ -19,29 +20,29 @@ class Advertisment(models.Model):
         return f'Advertisment(id = {self.id}, title = {self.title}, price = {self.price})'
     
 
-@admin.display(description="дата создания")
-def created_date(self):
-    from django.utils import timezone
-    from django.utils.html import format_html
+    @admin.display(description="дата создания")
+    def created_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
 
-    if self.created_at.date() == timezone.now().date():
-        create_time = self.created_at.time().strftime('%H:%M:%S')
-        return format_html(
-            '<span style - "color:green">Сегодня в {}</span>',create_time
-        )
-    return self.created_at.strftime('%d.%m.%Y in %H:%M:%S')
+        if self.created_at.date() == timezone.now().date():
+            create_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style - "color:green">Сегодня в {}</span>',create_time
+            )
+        return self.created_at.strftime('%d.%m.%Y in %H:%M:%S')
 
-@admin.display(description="дата обновления")
-def updated_date(self):
-    from django.utils import timezone
-    from django.utils.html import format_html
+    @admin.display(description="дата обновления")
+    def updated_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
 
-    if self.update_at.date() == timezone.now().date():
-        update_time = self.update_at.time().strftime('%H:%M:%S')
-        return format_html(
-            '<span style - "color:green">Сегодня в {}</span>',update_time
-        )
-    return self.update_at.strftime('%d.%m.%Y in %H:%M:%S')
+        if self.update_at.date() == timezone.now().date():
+            update_time = self.update_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style - "color:green">Сегодня в {}</span>',update_time
+            )
+        return self.update_at.strftime('%d.%m.%Y in %H:%M:%S')
 
 
 
